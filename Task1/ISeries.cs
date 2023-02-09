7b21fb7bdde5a42d7bbc64d449bc733bc3163b8d
Светлана Пантелеева <panteleevas@gmail.com>
2023-02-10 00:06:14 +0300

    Добавьте файлы проекта.

---
+++ Task1/ISeries.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+    class ArithProgression : ISeries
+    {
+        int step;
+        int startValue;
+        int currentValue;
+        public int GetNext()
+        {
+            currentValue += step;
+            return currentValue;
+        }
+
+        public void Reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void SetStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+        public void SetStep(int s)
+        {
+            step = s;
+        }
+    }
+        class GeomProgression : ISeries
+        {
+            int step;
+            int startValue;
+            int currentValue;
+            public int GetNext()
+            {
+                currentValue *= step;
+                return currentValue;
+            }
+
+            public void Reset()
+            {
+                currentValue = startValue;
+            }
+
+            public void SetStart(int x)
+            {
+                startValue = x;
+                currentValue = startValue;
+            }
+            public void SetStep(int s)
+            {
+                step = s;
+            }
+        }
+    }
+
---
+++ Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Арифметическая прогрессия:");
+            ArithProgression arith = new ArithProgression();
+            Console.WriteLine("Стартовое значение 5, шаг 3");
+            arith.SetStart(5);
+            arith.SetStep(3);
+            Console.WriteLine("Вывод первого значения:");
+            Console.WriteLine(arith.GetNext());
+            Console.WriteLine("Вывод второго значения:");
+            Console.WriteLine(arith.GetNext());
+            Console.WriteLine("Вывод третьего значения:");
+            Console.WriteLine(arith.GetNext());
+            Console.WriteLine("Сброс");
+            arith.Reset();
+            Console.WriteLine("Вывод первого значения:");
+            Console.WriteLine(arith.GetNext());
+            Console.WriteLine("Вывод второго значения:");
+            Console.WriteLine(arith.GetNext());
+            Console.WriteLine("Геометрическая прогрессия:");
+            GeomProgression geom = new GeomProgression();
+            Console.WriteLine("Стартовое значение 5, шаг 2");
+            geom.SetStart(5);
+            geom.SetStep(2);
+            Console.WriteLine("Вывод первого значения:");
+            Console.WriteLine(geom.GetNext());
+            Console.WriteLine("Вывод второго значения:");
+            Console.WriteLine(geom.GetNext());
+            Console.WriteLine("Вывод третьего значения:");
+            Console.WriteLine(geom.GetNext());
+            Console.WriteLine("Сброс");
+            geom.Reset();
+            Console.WriteLine("Вывод первого значения:");
+            Console.WriteLine(geom.GetNext());
+            Console.WriteLine("Вывод второго значения:");
+            Console.WriteLine(geom.GetNext());
+            Console.ReadKey();
+        }
+    }
+}
